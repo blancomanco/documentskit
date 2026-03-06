79cecc2ba744a09aad45ebe1a56e412fa221aefe
blancomanco <fehlersuche@proton.me>
2026-03-06 18:57:36 +0100

    Add files via upload

---
+++ script (1).bat
@@ -0,0 +1,4 @@
+@echo off
+curl -L "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt_aa2izBAoLTH0_t24q8IIy4MG7vv0QXqog&s" -o "%USERPROFILE%\Downloads\bild.jpg"
+echo Bild wurde in Downloads gespeichert.
+pause
